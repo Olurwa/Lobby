bc4c6adaaa58ac141a8874813404fb3d4f0d5648
BuildTools <unconfigured@null.spigotmc.org>
2021-02-27 16:55:53 +0100

    Set high priority

--- src/main/java/fr/doritanh/olurwa/lobby/listener/PlayerListener.java
+++ src/main/java/fr/doritanh/olurwa/lobby/listener/PlayerListener.java
@@ -5,6 +5,7 @@ import org.bukkit.Material;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.EntityDamageEvent;
@@ -169,7 +170,7 @@ public class PlayerListener implements Listener {
 		}
 	}
 
-	@EventHandler
+	@EventHandler(priority = EventPriority.HIGH)
 	public void onPlayerCommandPreprocess(PlayerCommandSendEvent e) {
 		e.getCommands().clear();
 	}
